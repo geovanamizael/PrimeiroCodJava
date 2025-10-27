3e86870960fbe7b3fb19d7912fd19883be9c5153
Geovana  Mizael <145933912+geovanamizael@users.noreply.github.com>
2025-10-26 22:08:23 -0300

    aprendendo sobre o uso da String para textos.

---
+++ PrimeiroCodJava/src/cursojava/iniciando/AprendendoString.java
@@ -0,0 +1,20 @@
+package cursojava.iniciando;
+
+public class AprendendoString {
+
+	public static void main(String[] args) {
+		
+		/*String em Java sempre será um texto de tamanho qualquer.*/
+		/*ele pode representar várias coisas e inicia com letra MAIÚSCULA*/
+
+		String textoQualquer = tudopodevirartexto; 
+		
+		/*é muito usado para cadastro. Exemplos abaixo*/
+		
+		String nome;
+		String cpf;
+		String telefone;
+		String endereco;
+		
+	}
+}
