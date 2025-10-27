378bf7b23f3eb438817f00d5a1994585322a916d
Geovana  Mizael <145933912+geovanamizael@users.noreply.github.com>
2025-10-27 15:30:07 -0300

    aprendendo concatenação de variáveis

---
+++ PrimeiroCodJava/src/cursojava/iniciando/ConcatenacaoVariaveis.java
@@ -0,0 +1,26 @@
+package cursojava.iniciando;
+
+public class ConcatenacaoVariaveis {
+
+	public static void main(String[] args) {
+		
+		
+		/*Concatenação de Variáveis -> Unir ou juntar dados*/
+		
+		String nome = "Geovana Mizael";
+		String cpf = "485.964.595.26";
+		String telefone = "11 94002-8922";
+		String endereco = "Av Santiago - SP ";
+		
+		/* como colocar tudo em uma linha só, em um console só*/ 
+		
+		System.out.println("Meu nome é: " + nome + " e meu cpf falso é: " +
+		cpf + ". Você pode entrar em contato comigo pelo número falso : " +
+				telefone + " ou pode ir ao meu endereço ficticio: " + endereco);
+		
+		 /*quebrar no "+". Aqui usamos o mais para concatenas as informações*/
+		
+		
+	}
+	
+}
