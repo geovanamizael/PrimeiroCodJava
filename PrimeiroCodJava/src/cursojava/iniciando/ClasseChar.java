52e85e619f9523889596502c7ddb39c830017a2a
Geovana  Mizael <145933912+geovanamizael@users.noreply.github.com>
2025-10-26 21:53:48 -0300

    Usando o Char pela primeira vez

---
+++ PrimeiroCodJava/src/cursojava/iniciando/ClasseChar.java
@@ -0,0 +1,24 @@
+package cursojava.iniciando;
+
+public class ClasseChar {
+
+	public static void main(String[] args) {
+		
+		/* CHAR representa uma letra/caracter --- usamos aspas SIMPLES*/
+		char pessoaFeminino = 'F';
+		
+		/*não posso colocar mais de uma letra, isso evita
+		 *  que alguém faça alguma alteração no código além do f*/
+		
+				
+				if (pessoaFeminino == 'F') {
+					System.out.println("Feminino");
+				}else {
+					System.out.println("Masculino");
+				}
+	}
+}
+
+/*posso representar varias coisas, por exemplo:
+ * char pessoaJuridico = 'J'
+ * char pessoa Fisica = 'F' */
